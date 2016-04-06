f9e29a73db37dfdc4e5a6a7fe5927e175e986b39
caodongping <caodongping@baidu.com>
2016-04-06 22:59:44 +0800

    router activity

---
+++ activityrouter/src/main/java/com/github/mzule/activityrouter/router/RouterActivity.java
@@ -0,0 +1,21 @@
+package com.github.mzule.activityrouter.router;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+
+/**
+ * Created by CaoDongping on 4/6/16.
+ */
+public class RouterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            Routers.getRouters().open(uri.toString());
+        }
+        finish();
+    }
+}
