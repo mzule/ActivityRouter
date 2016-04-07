037c1c2f515b44a73255ddfd0e9e6aa31707f2e6
caodongping <caodongping@baidu.com>
2016-04-07 11:22:10 +0800

    parse path segments arguments

--- activityrouter/src/main/java/com/github/mzule/activityrouter/router/Mapping.java
+++ activityrouter/src/main/java/com/github/mzule/activityrouter/router/Mapping.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -30,7 +31,11 @@ public class Mapping {
 
         Uri uri = Uri.parse("helper://".concat(format));
         this.host = uri.getHost();
-        this.pathArguments = uri.getPathSegments();
+        List<String> pathSegments = uri.getPathSegments();
+        pathArguments = new ArrayList<>(pathSegments.size());
+        for (String segment : pathSegments) {
+            pathArguments.add(segment.substring(1));
+        }
     }
 
     public String getFormat() {
@@ -71,34 +76,44 @@ public class Mapping {
     public Bundle parseExtras(String url) {
         Bundle bundle = new Bundle();
         Uri uri = Uri.parse(url);
+        // path segments
+        List<String> values = uri.getPathSegments();
+        for (int i = 0; i < pathArguments.size(); i++) {
+            put(bundle, pathArguments.get(i), values.get(i));
+        }
+        // parameter
         Set<String> names = uri.getQueryParameterNames();
         for (String name : names) {
             String value = uri.getQueryParameter(name);
-            int type = extraTypes.getType(name);
-            switch (type) {
-                case ExtraTypes.INT:
-                    bundle.putInt(name, Integer.parseInt(value));
-                    break;
-                case ExtraTypes.LONG:
-                    bundle.putLong(name, Long.parseLong(value));
-                    break;
-                case ExtraTypes.BOOL:
-                    bundle.putBoolean(name, Boolean.parseBoolean(value));
-                    break;
-                case ExtraTypes.SHORT:
-                    bundle.putShort(name, Short.parseShort(value));
-                    break;
-                case ExtraTypes.FLOAT:
-                    bundle.putFloat(name, Float.parseFloat(value));
-                    break;
-                case ExtraTypes.DOUBLE:
-                    bundle.putDouble(name, Double.parseDouble(value));
-                    break;
-                default:
-                    bundle.putString(name, value);
-                    break;
-            }
+            put(bundle, name, value);
         }
         return bundle;
+    }
+
+    private void put(Bundle bundle, String name, String value) {
+        int type = extraTypes.getType(name);
+        switch (type) {
+            case ExtraTypes.INT:
+                bundle.putInt(name, Integer.parseInt(value));
+                break;
+            case ExtraTypes.LONG:
+                bundle.putLong(name, Long.parseLong(value));
+                break;
+            case ExtraTypes.BOOL:
+                bundle.putBoolean(name, Boolean.parseBoolean(value));
+                break;
+            case ExtraTypes.SHORT:
+                bundle.putShort(name, Short.parseShort(value));
+                break;
+            case ExtraTypes.FLOAT:
+                bundle.putFloat(name, Float.parseFloat(value));
+                break;
+            case ExtraTypes.DOUBLE:
+                bundle.putDouble(name, Double.parseDouble(value));
+                break;
+            default:
+                bundle.putString(name, value);
+                break;
+        }
     }
 }
