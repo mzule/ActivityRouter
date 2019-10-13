b2d4724ee0b34269dfd72dd1560cae3f97524fbb
TonyTandAndroid <tonytangandroid@gmail.com>
2019-10-13 11:23:22 -0700

    Migrate to AndroidX

--- app_module/src/androidTest/java/com/github/mzule/activityrouter/module/ExampleInstrumentedTest.java
+++ app_module/src/androidTest/java/com/github/mzule/activityrouter/module/ExampleInstrumentedTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.junit.Assert.assertEquals;
 
